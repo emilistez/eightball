2ba5def50f8b88ba0e65ee5f6abc8f09b00fc61f
stezanator <estesney@gmail.com>
2024-05-15 22:53:13 -0600

    java script 8 ball code

--- main.js
+++ main.js
@@ -1,2 +1,39 @@
-let userName = '';
+let userName = 'Anthony';
 userName ? console.log(`Hello, ${userName}!`) : console.log('hello');
+
+let userQuestion = 'Will I win the lottery?';
+
+console.log(`${userName} has asked ${userQuestion}`);
+
+let randomNumber= Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+let eightBall = '';
+
+switch(randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+    break;
+    case 1:
+        eightBall ='It is decidely so';
+    break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+    break;
+    case 3:
+        eightBall = 'Cannot predict now';
+    break;
+    case 4:
+        eightBall = 'Don\'t count on it';
+    break;
+    case 5:
+        eightBall = 'My sources say no';
+    break;
+    case 6:
+        eightBall = 'Outlook not so good';
+    case 7:
+        eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(`The Magic 8 Ball says: ${eightBall}.`);
